49c6ca5a1ef168a262a1e88bf0f2798d3b7e1c79
Vitaliy <vitaliy@prisbank.com>
2020-02-26 14:22:48 +0200

    add auth

--- healthy.py
+++ healthy.py
@@ -1,15 +1,37 @@
 import databases
 import sqlalchemy
-from marshmallow import Schema, fields
+import uvicorn
 from starlette.applications import Starlette
 from starlette.config import Config
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 from starlette.responses import JSONResponse
 from starlette.routing import Route
-
+from starlette.requests import Request
+from authlib.integrations.starlette_client import OAuth
 
 config = Config('.env')
 DATABASE_URL = config('DATABASE_URL')
 metadata = sqlalchemy.MetaData()
+oauth = OAuth(config)
+oauth.register(
+    name='google',
+    access_token_url='https://www.googleapis.com/oauth2/v4/token',
+    authorize_url=('https://accounts.google.com/o/oauth2/v2/auth'
+                   '?access_type=offline'),
+    api_base_url='https://www.googleapis.com/',
+    client_kwargs={'scope': 'https://www.googleapis.com/auth/userinfo.email'},
+)
+oauth.register(
+    name='github',
+    access_token_url='https://github.com/login/oauth/access_token',
+    access_token_params=None,
+    authorize_url='https://github.com/login/oauth/authorize',
+    authorize_params=None,
+    api_base_url='https://api.github.com/',
+    client_kwargs={'scope': 'user:email'},
+)
 
 # Entities
 
@@ -26,6 +48,37 @@ database = databases.Database(DATABASE_URL)
 
 # Routing
 
+async def login(request: Request):
+    redirect_uri = 'https://api.healthy.adds.md/auth/' + request.path_params['provider']
+    if request.path_params['provider'] == "google":
+        return await oauth.google.authorize_redirect(request, redirect_uri)
+    if request.path_params['provider'] == "github":
+        return await oauth.github.authorize_redirect(request, redirect_uri)
+
+
+async def auth(request: Request):
+    if request.path_params['provider'] == "google":
+        token = await oauth.google.authorize_access_token(request)
+        user = await oauth.google.parse_id_token(request, token)
+        return JSONResponse(dict(user))
+
+
+app = Starlette(
+    debug=True,
+    routes=[
+        Route('/login/{provider}', endpoint=login),
+        Route('/auth/{provider}', endpoint=auth),
+    ],
+    on_startup=[database.connect],
+    on_shutdown=[database.disconnect],
+    middleware=[
+        Middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['*'], allow_headers=['*'], allow_credentials=True),
+        Middleware(SessionMiddleware, secret_key=config.get('SESSION_SECRET'))
+    ]
+)
+
+
+@app.route("/activities")
 async def list_activities(request):
     query = activities.select()
     results = await database.fetch_all(query)
@@ -37,10 +90,5 @@ async def list_activities(request):
     ])
 
 
-app = Starlette(
-    debug=True,
-    routes=[
-        Route('/activities', list_activities),
-    ],
-    on_startup=[database.connect],
-    on_shutdown=[database.disconnect])
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
